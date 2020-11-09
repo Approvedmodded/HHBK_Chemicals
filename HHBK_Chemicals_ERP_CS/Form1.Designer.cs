d611b36cc71d2aca2d765101946bf97bc7f9ba14
Edward Danilson <edward.danilson@gmx.de>
2020-11-09 12:08:39 +0100

    Nice one.

--- HHBK_Chemicals_ERP_CS/Form1.Designer.cs
+++ HHBK_Chemicals_ERP_CS/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HHBK_Chemicals_ERP_CS
 {
-    partial class Form1
+    partial class YEET
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Edward Danilson";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // YEET
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "YEET";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- HHBK_Chemicals_ERP_CS/Form1.cs
+++ HHBK_Chemicals_ERP_CS/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace HHBK_Chemicals_ERP_CS
 {
-    public partial class Form1 : Form
+    public partial class YEET : Form
     {
-        public Form1()
+        public YEET()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
